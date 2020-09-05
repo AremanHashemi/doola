697a1d139622b259d56308ff269be3089000223b
Cam Hashemi <camhashemi@gmail.com>
2020-09-05 15:13:05 +0200

    formatting: fix by running gofmt internal

--- internal/user/fake_repo.go
+++ internal/user/fake_repo.go
@@ -10,9 +10,9 @@ type FakeRepo struct {
 
 func (r FakeRepo) CreateUser(name string) User {
 	u := User{
-		Id: len(r.users),
+		Id:        len(r.users),
 		CreatedAt: time.Now(),
-		Name: name,
+		Name:      name,
 	}
 
 	r.users = append(r.users, u)
